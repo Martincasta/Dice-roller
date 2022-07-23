232bbb48ceffc7d86ed62c6af3a2c763195c8c6f
Martin Castañon <50704515+Martincasta@users.noreply.github.com>
2022-07-23 16:19:27 -0300

    Add files via upload
    Incorrectly uploaded the file with the last commit
    
    Added a lot of new functionalities to the roller

--- Dice roller/dice.js
+++ Dice roller/dice.js
@@ -1,17 +1,52 @@
 //--Bonus--//
 var bonus = document.querySelector("#bonus");
 
+var bonusValue = bonus.value;
+
 //--Amount of Die--//
 var amount = document.querySelector("#amount");
+
+var amountValue = amount.value;
 
 //--Type of Die--//
 
 var diceType = document.getElementById("dieType");
 
+var total = [];
+
+function anySides(){
+
+    var everySum = document.getElementById("Sum");
+    var everyResult = document.getElementById("Result");
+    //-- This is where the math is saved up, the amount of sides is decided by the user--//
+    var anySides = (Math.floor(Math.random()*(diceType.value)+1));
+
+    var total = [];
+    //-- This is where the die are rolled, always using the amount-1 for the math cause if it's 2 that's one die plus one die--//
+   for(var i = 1; i <= Number(amount.value); ++i){
+        
+    var firstDie = (Math.floor(Math.random()*(diceType.value)+1));
+
+    total.push(firstDie);
+
+    console.log(total  + "+" + i);}
 
 
-function rollDie(){
+    //-var severalDie = anySides + (Math.floor(Math.random()*(diceType.value)+1))*Number(amount.value-1);--//
+    //--If there's a bonus, it gets added here--//
+    var totalRoll = Number(total.reduce((a, b) => a + b, 0) + Number(bonus.value));
+    
+    everySum.innerHTML ="Your roll was: " + amount.value + "d"+diceType.value+"("+total+") + "  + bonus.value + " = " ;
+    everyResult.innerHTML = totalRoll;
 
+
+  
+}
+
+
+
+
+/*function rollDie(){
 
 switch(Number(diceType.value)){
 
@@ -19,15 +54,15 @@ switch(Number(diceType.value)){
     //--- D20 ---//
 case 20:
 
-    var d20Result = document.getElementById("Result");
+        var d20Result = document.getElementById("Result");
+        
+        var d20 = (Math.floor(Math.random()*20+1)) + Number((amount.value-1)*((Math.floor(Math.random()*20+1))));
     
-    var d20 = (Math.floor(Math.random()*20+1)) * amount.value ;
+        var sum = d20 + Number(bonus.value);
     
-    var sum = d20 + Number(bonus.value);
-    
-    d20Result.innerHTML = "Your d20 roll was: " + amount.value + "d20 + "  + bonus.value + " = " + sum;
+        d20Result.innerHTML = "Your d20 roll was: " + amount.value + "d20("+d20+") + "  + bonus.value + " = " + sum;
 
-    console.log(sum);
+        console.log(sum);
 
     break;
 
@@ -35,10 +70,10 @@ case 20:
     //--- D12 ---//
 case 12:
     var d12Result = document.getElementById("Result");
-    var d12 = Math.floor(Math.random()*12+1);
-    var sum = d12 + Number(bonus.value)
+    var d12 = Math.floor(Math.random()*12+1) + Number((amount.value-1)*((Math.floor(Math.random()*12+1))));
+    var sum = d12 + Number(bonus.value);
       
-    d12Result.innerHTML = "Your d12 roll was: " + d12 +" + " + bonus.value + " = " + sum;
+    d12Result.innerHTML = "Your d12 roll was: "  + amount.value + "d12 + " + bonus.value + " = " + sum;
 
     console.log(sum);
 
@@ -47,10 +82,10 @@ case 12:
     //--- D10 ---//
 case 10:
     var d10Result = document.getElementById("Result");
-    var d10 = Math.floor(Math.random()*10+1);
+    var d10 = Math.floor(Math.random()*10+1 + Number((amount.value-1)*((Math.floor(Math.random()*10+1)))));
     var sum = d10 + Number(bonus.value)
       
-    d10Result.innerHTML = "Your d10 roll was: " + d10 +" + " + bonus.value + " = " + sum;
+    d10Result.innerHTML = "Your d10 roll was: "  + amount.value + "d10 + " + bonus.value + " = " + sum;
 
     console.log(sum);
 
@@ -59,10 +94,10 @@ case 10:
     //--- D8 ---//
 case 8:
     var d8Result = document.getElementById("Result");
-    var d8 = Math.floor(Math.random()*8+1);
+    var d8 = Math.floor(Math.random()*8+1 + Number((amount.value-1)*((Math.floor(Math.random()*8+1)))));
     var sum = d8 + Number(bonus.value)
       
-    d8Result.innerHTML = "Your d8 roll was: " + d8 +" + " + bonus.value + " = " + sum;
+    d8Result.innerHTML = "Your d8 roll was: "  + amount.value + "d8 + " + bonus.value + " = " + sum;
 
     console.log(sum);
 
@@ -72,10 +107,10 @@ case 8:
     //--- D6 ---//
 case 6:
     var d6Result = document.getElementById("Result");
-    var d6 = Math.floor(Math.random()*6+1);
+    var d6 = Math.floor(Math.random()*6+1 + Number((amount.value-1)*((Math.floor(Math.random()*6+1)))));
     var sum = d6 + Number(bonus.value)
       
-    d6Result.innerHTML = "Your d6 roll was: " + d6 +" + " + bonus.value + " = " + sum;
+    d6Result.innerHTML = "Your d6 roll was: "  + amount.value + "d6 + " + bonus.value + " = " + sum;
 
     console.log(sum);
 
@@ -84,10 +119,10 @@ case 6:
     //--- D4 ---//
 case 4:
     var d4Result = document.getElementById("Result");
-    var d4 = Math.floor(Math.random()*4+1);
+    var d4 = Math.floor(Math.random()*4+1 + Number((amount.value-1)*((Math.floor(Math.random()*4+1)))));
     var sum = d4 + Number(bonus.value)
       
-    d4Result.innerHTML = "Your d4 roll was: " + d4 +" + " + bonus.value + " = " + sum;
+    d4Result.innerHTML = "Your d4 roll was: "  + amount.value + "d4 + " + bonus.value + " = " + sum;
 
     console.log(sum);
 
@@ -96,12 +131,12 @@ case 4:
 default:
     var defaultResult = document.getElementById("Result");
 
-    defaultResult.innerHTML = "Please choose a die number from d20 to d4"
+    defaultResult.innerHTML = "Please choose a die number from the set (d20,d12,d10,d8,d6,d4)"
 
 }
 
 };
-
+*/
 
 
 
